1d60ececa6f0072f7bc64c62c1ad1f9e1c4d58f7
mgross23 <m.gross23@gmail.com>
2026-03-21 23:03:49 -0400

    sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'finanzas-v3';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('firestore') ||
+      e.request.url.includes('firebase') ||
+      e.request.url.includes('googleapis')) {
+    e.respondWith(fetch(e.request).catch(() => new Response('', {status: 503})));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => {});
+      return cached || network;
+    })
+  );
+});
